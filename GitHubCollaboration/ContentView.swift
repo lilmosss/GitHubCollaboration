a36e4a3fd61adcd163d46ba203b8dbcc273fd32f
Lily Mosisa <lilymosisa13@gmail.com>
2025-05-05 18:36:18 -0400

    Initial Commit

---
+++ GitHubCollaboration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubCollaboration
+//
+//  Created by Lily Mosisa on 5/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Lily Mosisa on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
